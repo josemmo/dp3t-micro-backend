dd7f5e84d810b41c72575c64bb81267f6e555537
José Miguel Moreno <josemmo@pm.me>
2020-04-20 21:01:24 +0200

    Created install.php script
    - Created script for setting up the database structure
    - Removed "robmorgan/phinx" dependency

---
+++ install.php
@@ -0,0 +1,26 @@
+<?php
+require __DIR__ . "/bootstrap.php";
+
+use App\Utils\DB;
+
+// Get system information
+echo "[i] Fetching system information...\n";
+$engines = array_column(DB::getAll('SHOW ENGINES'), 'Engine');
+$supportsAria = in_array('Aria', $engines);
+
+// 2020-04-20 :: rev 1
+echo "[i] Creating 'exposees' table...\n";
+DB::query(
+    'CREATE TABLE IF NOT EXISTS exposees (
+        `key` binary(32) NOT NULL,
+        `onset` date NOT NULL,
+        `uploaded_at` datetime NOT NULL,
+        PRIMARY KEY (`key`),
+        KEY `onset` (`onset`),
+        KEY `upload_date` (`uploaded_at`)
+     ) ENGINE=?p DEFAULT CHARSET=utf8',
+    $supportsAria ? 'Aria' : 'InnoDB'
+);
+
+// Exit script
+echo "[i] Finished installing!\n";
